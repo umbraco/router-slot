97c0d8506cb470785b593ad37d6a40d679d10898
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-04-04 10:29:56 +0200

    update imports

--- src/demo/pages/home/secret/secret.ts
+++ src/demo/pages/home/secret/secret.ts
@@ -1,62 +1,69 @@
-import { html, LitElement, PropertyValues, TemplateResult } from "lit";
-import { IRouterSlot, PageComponent, query, IRoutingInfo } from "../../../../lib";
-import { ROUTER_SLOT_TAG_NAME } from "../../../../lib/config";
-import { sharedStyles } from "../../styles";
-import { data } from "./data";
+import { html, LitElement, PropertyValues, TemplateResult } from 'lit';
+import { ROUTER_SLOT_TAG_NAME } from '../../../../lib/config';
+import { sharedStyles } from '../../styles';
+import { data } from './data';
+import {
+  IRouterSlot,
+  PageComponent,
+  IRoutingInfo,
+} from '../../../../lib/model';
+import { query } from '../../../../lib/util';
 
-function resolveSecretPasswordGuard (): Promise<boolean> {
-	return new Promise(res => {
-		if (data.secretPassword != null) res(true);
-		setTimeout(() => {
-			data.secretPassword = `1234`;
-			res(true);
-		}, 1000);
-	});
+function resolveSecretPasswordGuard(): Promise<boolean> {
+  return new Promise((res) => {
+    if (data.secretPassword != null) res(true);
+    setTimeout(() => {
+      data.secretPassword = `1234`;
+      res(true);
+    }, 1000);
+  });
 }
 
 export default class SecretComponent extends LitElement {
-	static styles = [sharedStyles];
+  static styles = [sharedStyles];
 
-	firstUpdated (changedProperties: PropertyValues) {
-		super.firstUpdated(changedProperties);
+  firstUpdated(changedProperties: PropertyValues) {
+    super.firstUpdated(changedProperties);
 
-		const $routerSlot = this.shadowRoot!.querySelector<IRouterSlot>(ROUTER_SLOT_TAG_NAME)!;
-		$routerSlot.add([
-			{
-				path: "code",
-				component: () => import("./code/code"),
-				setup: (component: PageComponent, info: IRoutingInfo) => {
-					component.style.color = query()["yellow"] != null ? `yellow` : `blue`;
-				}
-			},
-			{
-				path: "password",
-				component: () => import("./password/password"),
-				guards: [resolveSecretPasswordGuard]
-			},
-			{
-				path: "**",
-				redirectTo: "code",
-				preserveQuery: true
-			}
-		]);
-	}
+    const $routerSlot =
+      this.shadowRoot!.querySelector<IRouterSlot>(ROUTER_SLOT_TAG_NAME)!;
+    $routerSlot.add([
+      {
+        path: 'code',
+        component: () => import('./code/code'),
+        setup: (component: PageComponent, info: IRoutingInfo) => {
+          component.style.color = query()['yellow'] != null ? `yellow` : `blue`;
+        },
+      },
+      {
+        path: 'password',
+        component: () => import('./password/password'),
+        guards: [resolveSecretPasswordGuard],
+      },
+      {
+        path: '**',
+        redirectTo: 'code',
+        preserveQuery: true,
+      },
+    ]);
+  }
 
-	/**
-	 * Renders the component.
-	 * @returns {TemplateResult}
-	 */
-	render (): TemplateResult {
-		return html`
-			<p>SecretComponent</p>
-			<router-link path="code">Go to CodeComponent</router-link>
-			<router-link path="password">Go to PasswordComponent (1sec delay)</router-link>
-			<div id="child">
-				<router-slot></router-slot>
-			</div>
-		`;
-	}
-
+  /**
+   * Renders the component.
+   * @returns {TemplateResult}
+   */
+  render(): TemplateResult {
+    return html`
+      <p>SecretComponent</p>
+      <router-link path="code">Go to CodeComponent</router-link>
+      <router-link path="password"
+        >Go to PasswordComponent (1sec delay)</router-link
+      >
+      <div id="child">
+        <router-slot></router-slot>
+      </div>
+    `;
+  }
 }
 
-window.customElements.define("secret-component", SecretComponent);
+window.customElements.define('secret-component', SecretComponent);
